c37a41366330fd891b456d6cd75515e04d89893c
Jeremy <jilammon10@gmail.com>
2025-12-06 15:43:45 -0800

    Day 3

--- day3.py
+++ day3.py
@@ -10,23 +10,19 @@ with open('day3_battery.txt') as file:
 def main():
     total = 0
     for bank in banks:
+        bank = str(int(bank))
         Highest_Index = 0
         Next_Highest_Index = 1
-        for i in range(len(bank)):
-            if bank[i] > bank[Highest_Index] and i != len(bank)-1:
+        for i in range(len(bank)-1):
+            if bank[i] > bank[Highest_Index]:
                 Highest_Index = i
-            if i > Highest_Index and Next_Highest_Index < Highest_Index:
+        Next_Highest_Index = i + 1
+        for i in range(Highest_Index+1, len(bank)):
+            if bank[i] > bank[Next_Highest_Index]:
                 Next_Highest_Index = i
-            elif bank[i] > bank[Next_Highest_Index] and i > Highest_Index:
-                Next_Highest_Index = i
-        if Next_Highest_Index <= Highest_Index:
-            print("WARNING IMPROPER NUMBER!")
-            print(Highest_Index, Next_Highest_Index)
-            print(bank)
         number = int(bank[Highest_Index] + bank[Next_Highest_Index])
-        print(number)
         total += number
-    print(total)
+    print("TOTAL:",total)
 
 
 
